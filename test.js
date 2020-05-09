18c2b63de158f1d023791d8b1e665345effbbd56
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-08 22:49:05 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+function asyncAction() {
+    var promise = new Promise(function (resolve, reject) {
+        resolve('finalizado');
+        reject('finalizado');
+    });
+    return promise;
+}
+asyncAction().then(function (input) {
+    console.log("control caso de ejecucion correcta " + input);
+})["catch"](function (error) {
+    console.log("control caso de ejecucion error " + error);
+});
